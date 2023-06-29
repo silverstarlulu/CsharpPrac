788c0c5cc769ceca41671eb49b7260a9b2065487
silverstarlulu <117253988+silverstarlulu@users.noreply.github.com>
2023-06-29 19:05:01 +0900

    1st prac

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+
+        class Car
+        {
+            private string handle="";
+
+            public void Run()
+            {
+                Console.WriteLine("달리기");
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("글자찍기");
+            Car car = new Car();
+            car.Run();
+        }
+    }
+}

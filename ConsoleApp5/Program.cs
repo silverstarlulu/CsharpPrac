8faf0ad240ada14f2587def3f459ddf5fcd57a3a
silverstarlulu <117253988+silverstarlulu@users.noreply.github.com>
2023-06-30 00:09:38 +0900

    조건문 및 반복문

---
+++ ConsoleApp5/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ConsoleApp5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 3;
+            int b = 10;
+
+            if (a < b)
+            {
+                Console.WriteLine("a보다 b가 더 큽니다.");
+            }
+            else if (a > b)
+            {
+                Console.WriteLine("b보다 a가 더 큽니다.");
+            }
+            else
+            {
+                Console.WriteLine("a와 b의 값은 같습니다.");
+            }
+
+            int week = 3;
+
+            switch (week)
+            {
+                case 1: 
+                    Console.WriteLine("월요일"); 
+                    break;
+                case 2: 
+                    Console.WriteLine("화요일"); 
+                    break;
+                case 3: 
+                    Console.WriteLine("수요일"); 
+                    break;
+                case 4: 
+                    Console.WriteLine("목요일"); 
+                    break;
+                default: 
+                    Console.WriteLine("금요일"); 
+                    break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //int i = 0;
+            //do
+            //{
+            //    Console.WriteLine(i);
+            //    i++;
+
+            //} while (i < 0);
+
+            //Console.WriteLine(i);
+
+            for(int i=0; i < 5; i++)
+            {
+                Console.WriteLine(i);
+             
+            }
+
+            string[] strs = { "blue", "pink", "orange", "gray" };
+
+            foreach(string str in strs)
+            {
+                Console.WriteLine(str);
+            }
+
+            for(int i=0; i < 3; i++)
+            {
+                for(int j=0; j < 2; j++)
+                {
+                    Console.WriteLine(i + "," + j);
+                }
+            }
+
+        }
+    }
+}

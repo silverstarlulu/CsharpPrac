b4fd401b7368eff9a2efbc3faae7aecbab945174
silverstarlulu <117253988+silverstarlulu@users.noreply.github.com>
2023-06-30 15:48:33 +0900

    로그인화면구현

---
+++ Doit_C_Sharp/WindowsSignIn/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsSignIn
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SignInButton_Click(object sender, EventArgs e)
+        {
+            string userId = id.Text;
+            string userPassword = password.Text;
+
+            if (userId.Equals("MyId") && userPassword.Equals("MyPassword"))
+            {
+                MessageBox.Show("로그인 성공", "로그인");
+            }
+            else
+            {
+                MessageBox.Show("로그인 실패", "로그인");
+            }
+        }
+    }
+}
